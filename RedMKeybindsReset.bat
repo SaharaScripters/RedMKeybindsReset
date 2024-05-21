a4a3a967248b8c8ea137236687672dd2326c9e05
MATech <81895695+maalouane@users.noreply.github.com>
2024-05-20 20:35:03 -0400

    Create RedMKeybindsReset.bat

---
+++ RedMKeybindsReset.bat
@@ -0,0 +1,33 @@
+@echo off 
+
+title Sahara Scripters RedM Keybinds Reset
+
+color 6
+echo Starting Resetting...
+timeout /t 3
+
+::RedM CFG File Deleting
+echo [Checking CitizenFX Directory]
+IF EXIST "%appdata%\CitizenFX\" (
+    echo [Deleting RedM CFG File]
+    cd /d "%appdata%\CitizenFX\
+    del redm.cfg
+    echo RedM CFG File Has Been Deleted Successfully.
+    timeout /t 3
+    cls
+    color 2
+    ::Final messages to show
+    echo.
+    echo  +------------------------------------+
+    echo  ^|                             		 ^|
+    echo  ^|[RedM CFG File Has Been Deleted] ^|
+    echo  ^|                        		     ^|
+    echo  +------------------------------------+
+    timeout /t 5
+) ELSE (
+    echo RedM CFG File Does Not Exist.
+    timeout /t 3
+    exit
+)
+
+exit
